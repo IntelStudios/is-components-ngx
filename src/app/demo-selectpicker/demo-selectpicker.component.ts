43f302ef0f59f7d14b0065580d665992a7a79162
Jaroslav Loutocky <jaroslav.loutocky@intelstudios.com>
2018-11-22 13:26:08 +0100

    update ng7 & selectpicker metronic v5

--- src/app/demo-selectpicker/demo-selectpicker.component.ts
+++ src/app/demo-selectpicker/demo-selectpicker.component.ts
@@ -10,7 +10,7 @@ export class DemoSelectpickerComponent implements OnInit {
   usage: string = `
 
 <h3>Installation</h3>
-<pre>npm install --save https://github.com/IntelStudios/is-components-ngx/raw/master/package/is-selectpicker-1.0.0.tgz</pre>
+<pre>npm install --save https://github.com/IntelStudios/is-components-ngx/raw/metronic_v5/package/is-selectpicker-1.0.0.tgz</pre>
 
 <h3>Import Module</h3>
 <pre>import { IsSelectpickerModule } from 'is-selectpicker';</pre>
