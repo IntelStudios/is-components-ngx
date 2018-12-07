04454d0360ec6d8488fc2ad8e42ab20dd4c877f1
Jaroslav Loutocky <jaroslav.loutocky@intelstudios.com>
2018-12-07 16:42:11 +0100

    is-password ControlValueAccessor

--- projects/is-password/src/lib/is-password.component.ts
+++ projects/is-password/src/lib/is-password.component.ts
@@ -1,9 +1,26 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  Output,
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+export const IS_PASSWORD_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => IsPasswordComponent),
+  multi: true
+};
 
 @Component({
   selector: 'is-password',
   templateUrl: 'is-password.component.html',
   styleUrls: ['is-password.component.scss'],
+  providers: [IS_PASSWORD_VALUE_ACCESSOR],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IsPasswordComponent {
@@ -17,11 +34,57 @@ export class IsPasswordComponent {
   @Input()
   title: string;
 
-  constructor() {
+  @Output()
+  changed: EventEmitter<any> = new EventEmitter<any>();
+
+  public value: string = '';
+
+  disabled: boolean;
+
+  private _changeSubscription: Subscription = null;
+  private onTouched: Function;
+
+  constructor(private changeDetector: ChangeDetectorRef) {
 
   }
 
   togglePassword(input: any) {
     input.type === 'text' ? input.type = 'password' : input.type = 'text';
+  }
+
+  /**
+   * Implemented as part of ControlValueAccessor.
+   */
+  writeValue(value: string): void {
+    if (!value) {
+      return;
+    }
+
+    this.value = value;
+  }
+
+  /**
+   * Implemented as part of ControlValueAccessor.
+   */
+  registerOnChange(fn: (_: any) => {}): void {
+    if (this._changeSubscription) {
+      this._changeSubscription.unsubscribe();
+    }
+    this._changeSubscription = this.changed.subscribe(fn);
+  }
+
+  /**
+   * Implemented as part of ControlValueAccessor.
+   */
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.changeDetector.markForCheck();
+  }
+
+  /**
+   * Implemented as part of ControlValueAccessor.
+   */
+  registerOnTouched(fn: (_: any) => {}): void {
+    this.onTouched = fn;
   }
 }
