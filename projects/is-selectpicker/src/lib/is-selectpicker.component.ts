1bd9ed9135965aa3cdeab01677f03865faeb0a43
Jaroslav Loutocky <jaroslav.loutocky@intelstudios.com>
2019-03-05 13:41:39 +0100

    fixed when item ID = 0 - selectpicker

--- projects/is-selectpicker/src/lib/is-selectpicker.component.ts
+++ projects/is-selectpicker/src/lib/is-selectpicker.component.ts
@@ -214,15 +214,13 @@ export class IsSelectpickerComponent implements ControlValueAccessor, OnInit, On
   writeValue(value: any): void {
     if (value instanceof Array) {
       this.values = (<any[]>value).map((i: any) => {
-        if (i.ID && i.Value) {
+        if (i.ID !== null && i.ID !== undefined && i.Value) {
           if (!this.useModels) {
-            console.error('[useModels] is enabled, but value is: ', value);
             throw new Error('[useModels] is not enabled, but you are trying to set model value')
           }
           return i;
         } else {
           if (this.useModels) {
-            console.error('[useModels] is enabled, but value is: ', value);
             throw new Error('[useModels] is enabled, but you are trying to set non-model value: ')
           }
           return {ID: i, Value: i, Object: null};
