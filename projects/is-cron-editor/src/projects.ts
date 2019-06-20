101e85a85a95ee5cbb0142b7daec575f07b8628a
Adam Hlaváček <git@adamhlavacek.com>
2019-06-20 10:09:17 +0200

    create is-cron-editor

---
+++ projects/is-cron-editor/src/projects.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of is-cron-editor
+ */
+
+export * from './lib/is-cron-editor.component';
+export * from './lib/is-cron-editor.module';
+export * from './lib/is-cron-editor.validator';
--- projects/is-selectpicker/src/lib/is-selectpicker.component.ts
+++ projects/is-selectpicker/src/lib/is-selectpicker.component.ts
@@ -255,7 +255,7 @@ export class IsSelectpickerComponent implements ControlValueAccessor, OnInit, On
 
     this.updateValueText();
 
-    this.changeDetector.detectChanges();
+    this.changeDetector.markForCheck();
   }
 
   /**
@@ -361,7 +361,7 @@ export class IsSelectpickerComponent implements ControlValueAccessor, OnInit, On
       this.valueText = '';
     }
 
-    this.changeDetector.detectChanges();
+    this.changeDetector.markForCheck();
   }
 
   private setActiveItem() {
