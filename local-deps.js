70c02f84ac29561be8e0e318872240f06c8997ed
Libor Zoubek <lzoubek@jezzovo.net>
2019-06-28 14:50:36 +0200

    build fix

--- local-deps.js
+++ local-deps.js
@@ -1,21 +1,7 @@
-// this script executes ng build for each project we have
-// each lib gets built into /dist
+// this script executes ng build for each project we have internal dependency
+// lib gets built into /dist
 
-const path = require('path');
-const fs = require('fs');
 const exec = require('child_process').execSync;
-//joining path of directory
-const directoryPath = path.join(__dirname, 'projects');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        const proj = path.join(directoryPath, file);
-        console.log('ng build '+file);
-        exec('ng build '+file);
-    });
-});
+exec('ng build is-select');
+exec('ng build is-selectpicker');
+exec('ng build is-metronic');
