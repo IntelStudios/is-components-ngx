035a8b24c0e4ddf49c6ca4422637d743dab77394
Libor Zoubek <lzoubek@jezzovo.net>
2019-07-26 15:09:26 +0200

    check-map: work in progress

--- projects/is-checkmap/src/lib/is-checkmap.component.ts
+++ projects/is-checkmap/src/lib/is-checkmap.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'lib-is-checkmap',
-  template: `
-    <p>
-      is-checkmap works!
-    </p>
-  `,
-  styles: []
+  selector: 'is-checkmap',
+  templateUrl: './is-checkmap.component.html',
+  styleUrls: ['./is-checkmap.component.scss']
 })
 export class IsCheckmapComponent implements OnInit {
 
-  constructor() { }
+  private elLeft: HTMLDivElement;
+  private elTop: HTMLDivElement;
+  constructor(private el: ElementRef, private renderer: Renderer2) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.elLeft = this.el.nativeElement.querySelector('.left');
+    this.elTop = this.el.nativeElement.querySelector('.top');
+  }
+
+  onScroll($event) {
+    this.elLeft.scrollTop = $event.target.scrollTop;
+    this.elTop.scrollLeft = $event.target.scrollLeft;
+  }
+
+  test() {
+
+    const left = this.el.nativeElement.querySelector('.left');
+    left.scrollTop = 100;
+  }
 }
