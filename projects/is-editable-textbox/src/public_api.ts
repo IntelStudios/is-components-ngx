1473e9b9daaa02ba9000156ec2585f6a304fd2e9
Adam Hlaváček <git@adamhlavacek.com>
2019-09-04 10:37:23 +0200

    add editable textbox (#9)
    * add editable textbox
    
    * remove is-input class from password

---
+++ projects/is-editable-textbox/src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of is-editable-textbox
+ */
+
+export * from './lib/is-editable-textbox.component';
+export * from './lib/is-editable-textbox.module';
---
+++ src/app/demo-editable-textbox/demo-editable-textbox/demo-editable-textbox.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-demo-editable-textbox',
+  templateUrl: './demo-editable-textbox.component.html',
+  styleUrls: ['./demo-editable-textbox.component.scss']
+})
+export class DemoEditableTextboxComponent implements OnInit {
+
+  usage = `
+
+<h3>Installation</h3>
+<pre>npm install --save https://github.com/IntelStudios/is-components-ngx/raw/7.x/package/is-editable-textbox-7.0.0.tgz</pre>
+
+<h3>Import Module</h3>
+<pre>import { IsEditableTextboxModule } from 'is-editable-textbox';</pre>`;
+
+  control = new FormControl();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.control.setValue('Click on the pen here');
+  }
+
+  writeMyName() {
+    setTimeout(() => this.control.setValue('My name'));
+  }
+
+  makeInvalid() {
+    this.control.setErrors({'this is just wrong': true});
+  }
+}
