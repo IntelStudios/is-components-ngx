95365ba78563bf3cdeebd8dbe2ef216ed3198e30
Libor Zoubek <lzoubek@jezzovo.net>
2019-10-09 15:09:13 +0200

    is-tabset nowrap feature + stretched feature

--- projects/is-core-ui/src/lib/is-tabset/is-tabset.component.ts
+++ projects/is-core-ui/src/lib/is-tabset/is-tabset.component.ts
@@ -10,8 +10,14 @@ import {
   Output,
   QueryList,
   TemplateRef,
+  OnInit,
+  OnDestroy,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, fromEvent, merge } from 'rxjs';
 
 let nextId = 0;
 
@@ -61,8 +67,8 @@ export class IsTabDirective {
    */
   @Input() load: 'always' | 'activeOnly' | 'selected' = 'always';
 
-  @ContentChild(IsTabContentDirective,  {static: true}) contentTpl: IsTabContentDirective;
-  @ContentChild(IsTabTitleDirective, {static: true}) titleTpl: IsTabTitleDirective;
+  @ContentChild(IsTabContentDirective, { static: true }) contentTpl: IsTabContentDirective;
+  @ContentChild(IsTabTitleDirective, { static: true }) titleTpl: IsTabTitleDirective;
 
   loaded: boolean = false;
 }
@@ -97,13 +103,17 @@ export interface TabChangeEvent {
   selector: 'is-tabset',
   styleUrls: ['is-tabset.component.scss'],
   template: `
-    <ul class="nav nav-tabs" role="tablist">
+    <ul class="nav nav-tabs" role="tablist" [class.stretched]="stretched">
       <li class="nav-item" *ngFor="let tab of tabs" [class.disabled]="tab.disabled">
         <a [id]="tab.id" class="nav-link {{tab.titleClass}}" [ngClass]="{'active show' : tab.id === activeId}" (click)="select(tab.id)">
           {{tab.title}}<ng-template [ngTemplateOutlet]="tab.titleTpl?.templateRef"></ng-template>
         </a>
       </li>
     </ul>
+
+    <div class="scroll-btn left"><i class="fas fa-chevron-circle-left" (click)="scrollTabRight()"></i></div>
+    <div class="scroll-btn right"><i class="fas fa-chevron-circle-right" (click)="scrollTabLeft()"></i></div>
+
     <div class="tab-content" *ngIf="tabs.length > 0">
       <ng-template ngFor let-tab [ngForOf]="tabs">
         <div class="tab-pane" [ngClass]="{'active show' : tab.id === activeId}" *ngIf="tab.loaded || tab.id === activeId" role="tabpanel" [attr.aria-labelledby]="tab.id">
@@ -113,24 +123,96 @@ export interface TabChangeEvent {
     </div>
   `,
 })
-export class IsTabsetComponent implements AfterContentChecked, AfterContentInit {
-  @ContentChildren(IsTabDirective) tabs: QueryList<IsTabDirective>;
-
+export class IsTabsetComponent implements AfterContentChecked, AfterContentInit, OnInit, OnDestroy, AfterViewInit {
   /**
    * An identifier of an initially selected (active) tab. Use the "select" method to switch a tab programmatically.
    */
   @Input() activeId: string;
 
+  /**
+   * Sets stretched display behavior. If Enabled, tab titless will stretch to max available space. If disabled
+   * tab titles will go from right to left
+   */
+  @Input() stretched: boolean = false;
+
   @Input() enableRouting: boolean;
+
+  /**
+   * Enable / disable tab wrap feature. If enabled, tab titles will remain in a single row with
+   * scroll. If disabled, tab titles will wrap.
+   */
+  @Input() nowrap: boolean = true;
 
   /**
    * A tab change event fired right before the tab selection happens. See NgbTabChangeEvent for payload details
    */
   @Output() tabChange = new EventEmitter<TabChangeEvent>();
+
+  @ContentChildren(IsTabDirective) tabs: QueryList<IsTabDirective>;
+
   private isSelecting: boolean = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  private elUL: HTMLUListElement;
 
+  private elBtnLeft: HTMLDivElement;
+  private elBtnRight: HTMLDivElement;
+
+  private _sub: Subscription;
+
+  constructor(private router: Router, private route: ActivatedRoute, private el: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngOnInit() {
+
+    if (this.nowrap) {
+      this.elUL = this.el.nativeElement.querySelector('ul');
+      this.elBtnLeft = this.el.nativeElement.querySelector('div.left');
+      this.elBtnRight = this.el.nativeElement.querySelector('div.right');
+
+      const sources = [
+        fromEvent(this.elUL, 'scroll', { passive: true }),
+        fromEvent(window, 'resize', { passive: true })
+      ];
+
+      this._sub = merge(...sources)
+        .subscribe(() => {
+          this.updateScrollBtnVisibility();
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this._sub) {
+      this._sub.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.updateScrollBtnVisibility();
+  }
+
+  ngAfterContentInit() {
+    //set internal loaded state based on input lazyLoad option
+    this.tabs.forEach((t: IsTabDirective) => {
+      if (!t.loaded) {
+        t.loaded = t.load === 'always';
+      } else {
+        t.loaded = t.load !== 'activeOnly';
+      }
+    });
+  }
+
+  ngAfterContentChecked() {
+    // // auto-correct activeId that might have been set incorrectly as input
+    let activeTab = this._getTabById(this.activeId);
+    if (activeTab) {
+      this.activeId = activeTab.id;
+    } else if (this.tabs.length) {
+      activeTab = this.tabs.first;
+      activeTab.loaded = true;
+      this.activeId = activeTab.id;
+    }
   }
 
   /**
@@ -171,26 +253,31 @@ export class IsTabsetComponent implements AfterContentChecked, AfterContentInit 
     }
   }
 
-  ngAfterContentInit() {
-    //set internal loaded state based on input lazyLoad option
-    this.tabs.forEach((t: IsTabDirective) => {
-      if (!t.loaded) {
-        t.loaded = t.load === 'always';
-      } else {
-        t.loaded = t.load !== 'activeOnly';
-      }
-    });
+  scrollTabRight() {
+    this.elUL.scrollBy({ behavior: 'smooth', left: -50 });
   }
 
-  ngAfterContentChecked() {
-    // // auto-correct activeId that might have been set incorrectly as input
-    let activeTab = this._getTabById(this.activeId);
-    if (activeTab) {
-      this.activeId = activeTab.id;
-    } else if (this.tabs.length) {
-      activeTab = this.tabs.first;
-      activeTab.loaded = true;
-      this.activeId = activeTab.id;
+  scrollTabLeft() {
+    this.elUL.scrollBy({ behavior: 'smooth', left: 50 });
+  }
+
+  private updateScrollBtnVisibility() {
+    if (!this.elUL) {
+      return;
+    }
+
+    if (this.elUL.scrollLeft === 0) {
+      this.renderer.setStyle(this.elBtnLeft, 'display', 'none');
+    } else {
+      this.renderer.removeStyle(this.elBtnLeft, 'display');
+      this.renderer.setStyle(this.elBtnLeft, 'height', `${this.elUL.clientHeight}px`);
+    }
+
+    if (this.elUL.scrollLeft === this.elUL.scrollWidth - this.elUL.clientWidth) {
+      this.renderer.setStyle(this.elBtnRight, 'display', 'none');
+    } else {
+      this.renderer.removeStyle(this.elBtnRight, 'display');
+      this.renderer.setStyle(this.elBtnRight, 'height', `${this.elUL.clientHeight}px`);
     }
   }
 
