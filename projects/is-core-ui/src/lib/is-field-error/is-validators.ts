add7b6882bbd71f68ad3b4ec84a13b515982cad5
Libor Zoubek <lzoubek@jezzovo.net>
2019-11-21 09:14:34 +0100

    is-field-error: add custom "required" validator

---
+++ projects/is-core-ui/src/lib/is-field-error/is-validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { IsFieldErrorFactory } from './is-field-error.service';
+
+export function isEmptyInputValue(value: any): boolean {
+  // we don't check for string here so it also works with arrays
+  return value == null || value.length === 0;
+}
+
+export class IsValidators {
+
+  static required(control: AbstractControl): ValidationErrors|null {
+    return isEmptyInputValue(control.value) ? IsFieldErrorFactory.requiredError() : null;
+  }
+
+
+
+}
--- projects/is-core-ui/src/public_api.ts
+++ projects/is-core-ui/src/public_api.ts
@@ -9,6 +9,7 @@ export * from './lib/is-core-ui.module';
 export * from './lib/is-field-error/is-field-error.component';
 export * from './lib/is-field-error/is-field-error.service';
 export * from './lib/is-field-error/is-field-error.model';
+export * from './lib/is-field-error/is-validators';
 export * from './lib/is-checkbox/is-checkbox.interfaces';
 export * from './lib/is-checkbox/is-checkbox.component';
 export * from './lib/is-search/is-search.component';
