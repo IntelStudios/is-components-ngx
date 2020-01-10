d27e6de817f8aaa098b997bbbc1d89da2d61c5df
Libor Zoubek <lzoubek@jezzovo.net>
2020-01-10 12:26:53 +0100

    is-modal support cdkScrollable

--- projects/is-modal/src/lib/is-modal.module.ts
+++ projects/is-modal/src/lib/is-modal.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { IsModalComponent } from './is-modal.component';
 
 @NgModule({
   imports: [
-    CommonModule, ModalModule, TranslateModule
+    CommonModule, ModalModule, TranslateModule, ScrollingModule
   ],
   declarations: [IsModalComponent],
   exports: [IsModalComponent],
