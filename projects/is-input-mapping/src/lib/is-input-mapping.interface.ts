44decc02ccb1d88c5104056c9eeedf225cd68e66
Adam Hlaváček <git@adamhlavacek.com>
2020-01-30 17:56:49 +0100

    input mapping: add JSON and Endpoint type with descriptions

--- projects/is-input-mapping/src/lib/is-input-mapping.interface.ts
+++ projects/is-input-mapping/src/lib/is-input-mapping.interface.ts
@@ -14,6 +14,7 @@ export interface DataStructure {
   Children: DataStructure[];
   Path: string;
   Name: string;
+  Description?: string;
   Type: number;
   DataType: number;
   InputColumns: string[];
