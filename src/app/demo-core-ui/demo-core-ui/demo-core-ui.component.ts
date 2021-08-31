e4933df284aea48332b3e2549a900f3004005e0d
Radek Bednarik <radek.bednarik@intelstudios.com>
2021-08-31 13:44:14 +0200

    is-tabset cdk scrolling module

--- src/app/demo-core-ui/demo-core-ui/demo-core-ui.component.ts
+++ src/app/demo-core-ui/demo-core-ui/demo-core-ui.component.ts
@@ -14,7 +14,7 @@ export class DemoCoreUIComponent implements OnInit {
   usage: string = `
 
 <h3>Installation</h3>
-<pre>npm install --save https://github.com/IntelStudios/is-components-ngx/raw/8.x/package/is-core-ui-8.1.7.tgz</pre>
+<pre>npm install --save https://github.com/IntelStudios/is-components-ngx/raw/8.x/package/is-core-ui-8.1.8.tgz</pre>
 
 <h3>Import Module</h3>
 <pre>import { IsCoreUIModule } from 'is-core-ui';</pre>
