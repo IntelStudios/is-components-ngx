c137aac4d06d7e28e652a71378113eebe36980c4
Radek Bednarik <radek.bednarik@intelstudios.com>
2022-05-12 14:32:49 +0200

    chore(is-cdk): add iframe-fonts directive

--- projects/is-cdk/src/lib/is-cdk.module.ts
+++ projects/is-cdk/src/lib/is-cdk.module.ts
@@ -5,13 +5,14 @@ import { IsIFrameResizeDirective } from './directives/iframe-resize.directive';
 import { PreventDoubleclickDirective } from './directives/prevent-dblclick.directive';
 import { IsIFrameClickDirective } from './directives/iframe-click.directive';
 import { IsCdkService } from './is-cdk.service';
+import { IsIFrameFontsDirective } from './directives/iframe-fonts.directive';
 
 @NgModule({
   imports: [
     CommonModule, OverlayModule,
   ],
-  declarations: [IsIFrameResizeDirective, PreventDoubleclickDirective, IsIFrameClickDirective],
-  exports: [IsIFrameResizeDirective, PreventDoubleclickDirective, IsIFrameClickDirective],
+  declarations: [IsIFrameResizeDirective, PreventDoubleclickDirective, IsIFrameClickDirective, IsIFrameFontsDirective],
+  exports: [IsIFrameResizeDirective, PreventDoubleclickDirective, IsIFrameClickDirective, IsIFrameFontsDirective],
   providers: [IsCdkService],
 })
 export class IsCdkModule { }
--- projects/is-cdk/src/public-api.ts
+++ projects/is-cdk/src/public-api.ts
@@ -5,6 +5,7 @@
 export * from './lib/is-cdk.service';
 export * from './lib/is-cdk.module';
 export * from './lib/directives/iframe-resize.directive';
+export * from './lib/directives/iframe-fonts.directive';
 export * from './lib/directives/prevent-dblclick.directive';
 export * from './lib/directives/iframe-click.directive'
 export * from './lib/is-field-error/is-field-error.service';
