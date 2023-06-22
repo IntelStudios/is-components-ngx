c6293e9b34e4855df456297e80b765a8c3749cc2
Libor Zoubek <libor.zoubek@intelstudios.com>
2023-06-22 15:19:12 +0200

    chore: local deps

--- tools/local-deps.js
+++ tools/local-deps.js
@@ -1,6 +1,6 @@
 // this script executes ng build for each project we have internal dependency
 // lib gets built into /dist
-const modules = ['is-cdk', 'is-core-ui', 'is-select', 'is-datepicker'];
+const modules = ['is-cdk', 'is-core-ui', 'is-select', 'is-timepicker', 'is-datepicker'];
 const exec = require('child_process').execSync;
 console.info(`Building local modules ${modules.length}`);
 modules.forEach(m => {
